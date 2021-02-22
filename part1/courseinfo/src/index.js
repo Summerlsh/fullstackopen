a209b6638b46d27c0142ad2ab79ad75c9a7a3261
lsh <496224211@qq.com>
2021-02-22 17:04:57 +0800

    Exercises 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -16,17 +16,19 @@ const Content = () => {
   const exercises3 = 14
 
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
 
