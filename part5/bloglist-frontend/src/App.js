7895cf790cf0acd4bab354b1df6040548ef56675
lsh <496224211@qq.com>
2021-03-11 19:14:35 +0800

    Exercises 5.6

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -6,14 +6,12 @@ import Blog from './components/Blog'
 import Notification from './components/Notification'
 import LoginForm from "./components/LoginForm";
 import Toggleable from "./components/Toggleable";
+import BlogForm from "./components/BlogForm";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [message, setMessage] = useState('')
   const [messageType, setMessageType] = useState('')
   const [user, setUser] = useState(null)
@@ -40,19 +38,13 @@ const App = () => {
     }
   }
 
-  const handleAddBlog = async event => {
-    event.preventDefault()
-
+  const addBlog = async (blogObject) => {
     try {
-      const newBlog = await blogService.createBlog({
-        title, author, url
-      })
+      const newBlog = await blogService.createBlog(blogObject)
+
       setMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
       setMessageType('success')
       setBlogs(blogs.concat(newBlog))
-      setTitle('')
-      setAuthor('')
-      setUrl('')
     } catch (err) {
       const res = err.response.data
       setMessage(`${res.error}`)
@@ -67,30 +59,6 @@ const App = () => {
     window.localStorage.removeItem('loggedUser')
     setUser(null)
   }
-
-  const CreateBlogForm = () => (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={handleAddBlog}>
-        <div>
-          title: <input value={title}
-                        type="text"
-                        onChange={({target}) => setTitle(target.value)}/>
-        </div>
-        <div>
-          author: <input value={author}
-                         type="text"
-                         onChange={({target}) => setAuthor(target.value)}/>
-        </div>
-        <div>
-          url: <input value={url}
-                      type="text"
-                      onChange={({target}) => setUrl(target.value)}/>
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
 
   useEffect(() => {
     const fetchData = async () => {
@@ -123,7 +91,7 @@ const App = () => {
         <Notification text={message} type={messageType}/>
         <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
         <Toggleable buttonLabel='new note'>
-          <CreateBlogForm/>
+          <BlogForm createBlog={addBlog}/>
         </Toggleable>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog}/>
---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+
+const BlogForm = ({createBlog}) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    createBlog({
+      title, author, url
+    })
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          title: <input value={title}
+                        type="text"
+                        onChange={({target}) => setTitle(target.value)}/>
+        </div>
+        <div>
+          author: <input value={author}
+                         type="text"
+                         onChange={({target}) => setAuthor(target.value)}/>
+        </div>
+        <div>
+          url: <input value={url}
+                      type="text"
+                      onChange={({target}) => setUrl(target.value)}/>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+}
+
+export default BlogForm
