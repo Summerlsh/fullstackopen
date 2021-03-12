073abeb48d4ca6917cc9407a942360884705dc51
Shawn <496224211@qq.com>
2021-03-12 14:25:23 +0800

    Exercises 5.7

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -96,7 +96,7 @@ const App = () => {
         </Toggleable>
 
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog}/>
+          <Blog key={blog.id} blog={blog} user={user}/>
         )}
       </div>
     )
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog, user}) => {
+  const [show, setShow] = useState(false)
+
+  const buttonText = show ? 'hide' : 'view'
+  const detailStyle = {display: show ? '' : 'none'}
+  const blogStyle = {
+    border: '2px solid',
+    marginBottom: 5,
+    paddingTop: 10,
+    paddingLeft: 2
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShow(!show)}>{buttonText}</button>
+      </div>
+      <div style={detailStyle}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{user.name}</div>
+      </div>
+    </div>
+  );
+}
 
 export default Blog
