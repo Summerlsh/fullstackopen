2a3e17a04353d5d20ad6b899dc935f070736418e
Shawn <496224211@qq.com>
2021-03-15 14:17:16 +0800

    Exercises 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog\'s title and author', () => {
@@ -19,7 +19,31 @@ test('renders blog\'s title and author', () => {
   const component = render(
     <Blog blog={blog}/>
   )
-  component.debug()
   expect(component.container.querySelector('.blog')).toBeDefined()
   expect(component.container).toHaveTextContent('Alice')
+})
+
+test('It should show url and likes after clicking the button', () => {
+  const blog = {
+    title: 'Test',
+    author: 'Alice',
+    url: 'http://localhost:3000',
+    likes: 10,
+    user: {
+      id: '123',
+      name: 'test',
+      username: 'test'
+    }
+  }
+
+  const component = render(
+    <Blog blog={blog}/>
+  )
+
+  expect(component.container).not.toHaveTextContent('likes')
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('http://localhost:3000')
+  expect(component.container).toHaveTextContent('likes')
 })
