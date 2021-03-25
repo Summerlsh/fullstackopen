93c8350e07bbb106d29da07cf9a45506333eae31
Shawn <496224211@qq.com>
2021-03-25 11:21:22 +0800

    Exercises 7.14

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
 
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import { getLoggedInUser, logout } from './reducers/loginReducer'
 import BlogList from './components/BlogList'
 import Users from './components/Users'
+import UserDetails from './components/UserDetails'
 
 const App = () => {
   const dispatch = useDispatch()
   const notification = useSelector((state) => state.notification)
   const loggedInUser = useSelector((state) => state.loggedInUser)
+  const matchedUser = useRouteMatch('/users/:id')
 
   useEffect(() => {
     dispatch(getLoggedInUser())
@@ -37,8 +39,11 @@ const App = () => {
         <Route exact path="/">
           <BlogList/>
         </Route>
-        <Route path="/users">
+        <Route exact path="/users">
           <Users/>
+        </Route>
+        <Route path="/users/:id">
+          <UserDetails id={matchedUser?.params.id}/>
         </Route>
       </Switch>
     </div>
