535e2ccf1f95b05b328a6ecfed6e507031016f20
Shawn <496224211@qq.com>
2021-03-29 14:07:48 +0800

    feat: Exercises 8.8-8.9

---
+++ part8/library-frontend/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es2021': true,
+    'jest/globals': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react', 'jest'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'react/prop-types': 0
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  }
+}
