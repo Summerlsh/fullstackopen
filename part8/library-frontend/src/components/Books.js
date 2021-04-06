ed918bc6292f79652a25029284ce64133d5000d6
Shawn <496224211@qq.com>
2021-04-06 14:03:10 +0800

    feat: Exercises 8.17

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client'
 
 import { ALL_BOOKS } from '../queries'
 
-const Books = (props) => {
+const Books = props => {
   const { loading, data } = useQuery(ALL_BOOKS)
   if (!props.show) {
     return null
@@ -24,10 +24,10 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {data.allBooks.map((a) => (
+          {data.allBooks.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           ))}
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -14,25 +14,17 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
 `
 
 export const CREATE_BOOK = gql`
-  mutation createBook(
-    $title: String!
-    $author: String!
-    $published: Int!
-    $genres: [String!]!
-  ) {
-    addBook(
-      title: $title
-      author: $author
-      published: $published
-      genres: $genres
-    ) {
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
       title
       author
       published
